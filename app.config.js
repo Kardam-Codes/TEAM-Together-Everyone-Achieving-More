36ed4b5c899f5b1b0e08f855869748025886aa09
vheet91-debug <vheet91@gmail.com>
2026-04-19 01:17:58 +0530

    Add app logo for icon and splash

--- app.config.js
+++ app.config.js
@@ -9,6 +9,18 @@ export default {
     version: '1.0.0',
     orientation: 'portrait',
     userInterfaceStyle: 'light',
+    icon: './mobile/src/logo.png',
+    splash: {
+      image: './mobile/src/logo.png',
+      resizeMode: 'contain',
+      backgroundColor: '#FFFFFF',
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: './mobile/src/logo.png',
+        backgroundColor: '#FFFFFF',
+      },
+    },
     assetBundlePatterns: ['**/*'],
     experiments: {
       tsconfigPaths: true,
